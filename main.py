3dd4f3d12f02cf67d391d8dd6ea3b6048d856479
Ryan Rudes <ryanrudes@gmail.com>
2021-02-15 13:17:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from rl.memory import SequentialMemory
 from rl.random import OrnsteinUhlenbeckProcess
 
 import wandb
-wandb.init(project = 'slitherio', entity = 'ryan-rudes')
+wandb.init(project = input("Enter your wandb project name: "), entity = input("Enter your wandb entity name: "))
 
 from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
@@ -15,6 +15,7 @@ import os
 
 from replay_buffer import ReplayBuffer
 from action_noise import OUActionNoise
+from frame_stack import FrameStack
 from slither import Slitherio
 from model import *
 from utils import *
@@ -22,12 +23,12 @@ from utils import *
 start = time.time()
 os.mkdir(f'./models/{start}/')
 
-max_memory_size = 50000
-minibatch_size = 64
-gamma = 0.999
+max_memory_size = 1000000
+minibatch_size = 16
+gamma = 0.99
+tau = 0.001
 std_dev = 0.2
 noise_generator = OUActionNoise(mean=np.zeros(1), std_deviation=float(std_dev) * np.ones(1))
-save_frequency = 10
 
 actor = make_actor()
 critic = make_critic()
@@ -47,17 +48,13 @@ actor_target_vars = actor_target.trainable_variables
 critic_vars = critic.trainable_variables
 critic_target_vars = critic_target.trainable_variables
 
-actor_lr = 0.001
-critic_lr = 0.002
+actor_lr = 0.0001
+critic_lr = 0.001
 
 actor_optimizer = Adam(actor_lr)
 critic_optimizer = Adam(critic_lr)
 
-gamma = 0.99
-tau = 0.005
-
 replay_memory = ReplayBuffer(max_memory_size)
-optimizer = Adam(lr = 0.001, clipnorm = 1.0)
 
 def act(observation, length, noise_generator):
     sampled_actions = tf.squeeze(actor([tf.expand_dims(observation, axis = 0), tf.expand_dims(length, axis = 0)]))
@@ -66,16 +63,16 @@ def act(observation, length, noise_generator):
     return sampled_actions
 
 env = Slitherio(nickname = "Bot")
+env = FrameStack(env)
 env.start()
 
 updates = 0
 episodes = 0
 highscore = 0
 longest_duration = 0
-angle = 0
 
 while True:
-    observation = preprocess(env.reset())
+    observation = env.reset()
     length = env.score
     terminal = False
     mean_actor_loss = 0
@@ -84,13 +81,9 @@ while True:
     timestep = 0
     while not terminal:
         action = act(observation, length, noise_generator)
-        delta_angle, acceleration = action
-        angle += delta_angle
-        if abs(angle) > 1:
-            angle %= 1
+        angle, acceleration = action
         acceleration = int(acceleration > 0)
         next_observation, reward, terminal, info = env.step(angle, acceleration)
-        next_observation = preprocess(next_observation)
         next_length = env.score
         replay_memory.store(observation, length, action, reward, next_observation, next_length)
         observation = next_observation
